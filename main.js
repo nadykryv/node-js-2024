280819dff73c4286698d75c7ea9c0f025d846282
Nadykryv <nkryvonis@gmail.com>
2024-12-14 17:57:22 +0100

    add hw2

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-//const {foo} = require('./helpers/helper')
+const path = require('node:path');
+const fs = require('node:fs/promises');
 
-//console.log('_______main.js______');
-//console.log(__dirname);
-//console.log(__filename);
-//console.log(process.cwd());
-
-//foo()
-const readline = require('node:readline');
 const foo = async () => {
-    const rlInstance = readline.createInterface( {
-        input: process.stdin,
-        output: process.stdout
-    })
+    const pathToFile = path.join(__dirname, 'baseFolder');
+    await fs.mkdir(pathToFile, { recursive: true });
 
+    const folders = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
+    const files = ['file1.txt', 'file2.txt' , 'file3.txt' , 'file4.txt' , 'file5.txt'];
 
-    rlInstance.question('What is your name?', (name) => {
-        console.log(`Hello, ${name}`);
-        rlInstance.close();
+    await Promise.all(folders.map(async (folder) => {
+        const folderPath = path.join(pathToFile, folder);
+        await fs.mkdir(folderPath, { recursive: true });
 
-    })
+        await Promise.all(files.map(async (file) => {
+            await fs.writeFile(path.join(folderPath, file), 'Hello Word');
+        }));
+    }));
+
+    const data = await fs.readdir(pathToFile);
+    for (const folder of data) {
+        const folderPath = path.join(pathToFile, folder);
+        const files = await fs.readdir(folderPath);
+        const stat = await fs.stat(folderPath);
+        console.log(`file: ${folderPath} - Is File: ${stat.isFile()}`);
+
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const stat = await fs.stat(filePath);
+            console.log(`file: ${filePath} - Is File: ${stat.isFile()}`);
+        }
+    }
 
 }
 void foo();
