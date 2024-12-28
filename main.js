c69057c29ea535ba3ac813fbffbade3a727f78b7
Nadykryv <nkryvonis@gmail.com>
2024-12-29 00:48:49 +0100

    add hw 3

--- main.js
+++ main.js
@@ -1,35 +1,88 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const {read, write} = require('./fs.service')
 dotenv.config({ path: '.env' })
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-app.get('/users', (req, res) => {
-    res.json(users);
-});
-app.post('/users', (req, res) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+app.get('/users', async (req, res) => {
+    try {
+        const users = await read();
+        res.json(users);
+    }catch(e) {
+        res.status( 500).json(e.message);
     }
-    users.push(newUser)
-    res.status(201).json(newUser);
 });
-app.get('/users/:userId', (req, res) => {
-    console.log('params:',req.params);
-    console.log('query:',req.query);
-    console.log('body:',req.body);
-    const user = users.find(user=> user.id === +req.params.userId);
-    res.json(user);
+
+app.post('/users', async (req, res) => {
+    try {
+        const users = await read();
+        const newUser = {
+            id: users.length ? users[users.length - 1].id + 1 : 1,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }
+        users.push(newUser);
+        await write(users);
+        res.status(201).json(newUser);
+    }catch(e) {
+        res.status( 500).json(e.message);
+    }
 });
-app.delete('/users/:userId', (req, res) => {
-    users = users.filter(user => user.id !== Number(req.params.userId));
-    res.sendStatus(204);
+
+app.get('/users/:userId', async (req, res) => {
+    try{
+        const users = await read();
+
+        const user = users.find(user => user.id === +req.params.userId);
+        res.json(user);
+    }catch(e) {
+        res.status( 500).json(e.message);
+    }
 });
+
+app.put('/users/:userId', async (req, res) => {
+    try{
+        const users = await read();
+        const index = users.findIndex(user => user.id === Number(req.params.userId));
+
+        if (index === -1) {
+            return res.status(404).json('User Not Found');
+        }
+        const user = users[index];
+            user.name = req.body.name;
+            user.email = req.body.email;
+            user.password = req.body.password;
+
+        await write(users);
+        res.status(201).json(user);
+    }catch(e) {
+        res.status( 500).json(e.message);
+    }
+
+});
+app.delete('/users/:userId', async (req, res) => {
+    try{
+        const users = await read();
+        const index = users.findIndex(user => user.id === Number(req.params.userId));
+
+        if (index === -1) {
+            return res.status(404).json({message: 'user not found'});
+        }
+        console.log(index);
+        users.splice(index, 1);
+        await write(users);
+        res.sendStatus(204);
+    }catch(e) {
+        res.status( 500).json(e.message);
+    }
+});
+
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
